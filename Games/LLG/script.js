2ddf224cc5ffe91d43fe1253df8d900b971e806e
mukeshJuadi <141830086+gameerector@users.noreply.github.com>
2023-09-14 12:39:41 +0530

    T ShareWeb

--- Games/LLG/script.js
+++ Games/LLG/script.js
@@ -706,21 +706,30 @@ captureAndShareButton.addEventListener('click', () => {
         // Create a shareable file from the Blob
         const shareableFile = new File([blob], 'screenshot.png', { type: 'image/png' });
         const myResult = 'I scored ' + PercentageValue.toFixed(2) + '% in the Learn Lingo Game!';
-        const shareText = myResult + '\n\nCheck out my result: https://eduplayzone.online/Games/LLG/';
+        const shareText = myResult + '\n\nCan you beat my score and become the ultimate language master? 🚀';
   
-        // Create an array to hold both text and files
-        const shareData = [shareText, shareableFile];
-  
-        // Share the text and the image using the built-in share dialog
+        // Try to share both text and image
         if (navigator.share) {
           navigator
             .share({
               title: 'Check out my Learn Lingo result!',
               text: shareText,
+              url: 'https://eduplayzone.online/Games/LLG/', // Add the URL you want to share
               files: [shareableFile],
             })
             .then(() => console.log('Shared successfully'))
-            .catch((error) => console.error('Error sharing:', error));
+            .catch((error) => {
+              console.error('Error sharing with text and image:', error);
+              // If sharing with text and image fails, try sharing only the image
+              navigator
+                .share({
+                  title: 'Check out my Learn Lingo result!',
+                  url: 'https://eduplayzone.online/Games/LLG/', // Add the URL you want to share
+                  files: [shareableFile],
+                })
+                .then(() => console.log('Shared successfully (image only)'))
+                .catch((error) => console.error('Error sharing image only:', error));
+            });
         } else {
           // Fallback for browsers that do not support Web Share API
           alert('Web Share API is not supported in your browser. You can manually share the text.');
